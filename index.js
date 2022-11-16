c7d7f05007e0830213218c653f5d2ba054816ad1
heahgo <imcastleempty@gmail.com>
2022-11-16 17:41:26 +0900

    페이지 내용 추가

--- index.js
+++ index.js
@@ -25,4 +25,4 @@ app.use((req, res, next) => {
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
--- routes/home.js
+++ routes/home.js
@@ -1,4 +1,5 @@
-const express = require('express')
+
+const express = require('express');
 const path = require('path');
 var router = express.Router(); // Declare an object using a functional module 
 
@@ -29,12 +30,59 @@ router.get('/community', function(req, res){
   res.render('home/community');
 });
 
-router.get('/quiz', function(req, res){
-  res.render('home/quiz');
-});
+router.route('/quiz')
+  .get(function(req, res) {
+    const answer = req.query['answer'];
+    if (answer === undefined) res.render('home/quiz');
+    else if (answer == 2) {
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+      res.write("<script>alert('정답입니다.')</script>");
+      res.write("<script>window.location='/eduBasic'</script>");
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+      res.write("<script>alert('틀렸습니다.')</script>");
+      res.write("<script>window.location='/quiz'</script>");
+    }
+    });
+// router.route('/quiz')
+//   .get(function(req, res){
+//     const answer = req.query['answer'];
+//     let quiz_num = req.query['num'];
+//     if (quiz_num == undefined) {
+//       quiz_num = 1;
+//     }
+//     const quizJson = `{
+//       "1": {
+//         "query": "Q. 1 + 1 = ?",
+//         "exam1": "1",
+//         "exam2": "2",
+//         "exam3": "3",
+//         "exam4": "4",
+//         "correct": "2"
+//       },
+//       "2": {
+//           "query": "Q. 14 - 9  = ?",
+//           "exam1": "5",
+//           "exam2": "6",
+//           "exam3": "7",
+//           "exam4": "8",
+//           "correct": "1"
+//         }
+//       }`;
+//     const quiz = JSON.parse(quizJson);
+//     if (answer == quiz[quiz_num].correct) {
+//       res.write("<script>alert('정답입니다.')</script>");
+//       quiz_num = parseInt(quiz_num) + 1;
+//     }
+//     res.render('home/quiz', quiz[quiz_num])
+//   });
 
 router.get('/goPqc', function(req, res){
   res.render('home/goPqc');
+});
+
+router.get('/nist', function(req, res){
+  res.render('home/nist');
 });
 
 module.exports = router;
